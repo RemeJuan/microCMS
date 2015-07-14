6a46fdade4ae6befa3b7f997fc0382da54e53999
Reme Le Hane <rlehane@tagworldiwde.com>
2015-07-14 12:48:40 +0200

    Fixed variable name

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var config = {};
 config.mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/micro-cms';
 
 //Site details
-config.sitename = 'Reme Le Hane';
+config.siteName = 'Reme Le Hane';
 
 //Cache
 config.coockieMaxAge = 30 * 24 * 3600 * 1000;
