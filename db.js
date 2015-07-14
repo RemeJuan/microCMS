535075deeb12b0f5b127212c4f05b1e4d3f0b254
Reme Le Hane <rlehane@tagworldiwde.com>
2015-07-14 12:31:30 +0200

    Removed old db file

--- db.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost:27017/ang');
-var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function(){
-
-});
